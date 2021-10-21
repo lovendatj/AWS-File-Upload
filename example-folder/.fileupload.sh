5e7ccbbce7e9ea62fa959e33114878718efa3a1c
John Murray <69658747+lovendatj@users.noreply.github.com>
2021-10-21 11:56:22 -0400

    file upload simulation

--- example-folder/.fileupload.sh
+++ example-folder/.fileupload.sh
@@ -59,9 +59,9 @@ clean_up(){
 file_upload(){
     if [ ! -z $filterFile ]
     then
-        py test.py $readPath $bucketAlias $filterFile 
+        py test.py readPath=$readPath bucketAlias=$bucketAlias filter=$filterFile 
     else    
-        py test.py $readPath $bucketAlias
+        py test.py readPath=$readPath bucketAlias=$bucketAlias
     fi
 }
 
